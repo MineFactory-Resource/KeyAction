3ad87fcb21d3b4cf2f0b0e3216ead3af1a60c590
Seokhun-Yang <ddayang1234@gmail.com>
2022-01-08 23:39:01 +0700

    KEYAC-2 fourth modification

--- src/main/java/net/teamuni/shiftf/Shiftf.java
+++ src/main/java/net/teamuni/shiftf/Shiftf.java
@@ -1,7 +1,6 @@
 package net.teamuni.shiftf;
 
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
